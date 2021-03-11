7b5148dc2c49fe4eedf2de9de75cc4a488d6354c
Calvin Houser <49534966+xeroxcat@users.noreply.github.com>
2021-03-11 09:59:34 -0800

    Add files via upload

---
+++ spacemax-dark-theme.el
@@ -0,0 +1,7 @@
+(require 'spacemax-common "~/.doom.d/themes/spacemax-common.el")
+
+(deftheme spacemax-dark "Spacemax theme, the dark version")
+
+(create-spacemax-theme 'dark 'spacemax-dark)
+
+(provide-theme 'spacemax-dark)
